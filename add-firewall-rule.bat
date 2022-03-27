9f732ed61f4c398541e66f582c72548a53dc249c
Alice <minnowbrine@live.ca>
2022-03-27 12:26:06 -0400

    added firewall rule bat scripts
    default front end port = 722
    default back end port = 721

---
+++ add-firewall-rule.bat
@@ -0,0 +1,2 @@
+netsh advfirewall firewall add rule name="4Nyan-Backend" dir=in action=allow protocol=TCP localport=721
+netsh advfirewall firewall add rule name="4Nyan-Frontend" dir=in action=allow protocol=TCP localport=722
---
+++ remove-firewall-rule.bat
@@ -0,0 +1,2 @@
+netsh advfirewall firewall delete rule name="4Nyan-Backend"
+netsh advfirewall firewall delete rule name="4Nyan-Frontend"
