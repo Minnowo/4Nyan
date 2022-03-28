ebdd1a70ce013767b922d2240f9b1ba012cc2223
Minnowo <minnowbrine@live.ca>
2022-03-28 13:49:49 -0400

    gitignore

--- frontend/client/src/components/Default.js
+++ frontend/client/src/components/Default.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class Default extends Component {
   render() {
     return (
-      <div>Welcome to /, change me!</div>
+      <div>
+         <video controls>
+            <source src="http://192.168.1.149:721/static/v/fall2.mp4" type="video/mp4"></source>
+            Your browser does not support the video tag.
+          </video> 
+      </div>
     )
   }
 }
